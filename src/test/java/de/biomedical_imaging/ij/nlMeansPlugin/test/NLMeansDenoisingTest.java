a054a950b79ce6592ebab16bfa53ab78fad6e4a9
jumpfunky <thotho@posteo.de>
2016-01-14 12:51:08 +0100

    Release 1.4.4

--- src/test/java/de/biomedical_imaging/ij/nlMeansPlugin/test/NLMeansDenoisingTest.java
+++ src/test/java/de/biomedical_imaging/ij/nlMeansPlugin/test/NLMeansDenoisingTest.java
@@ -24,6 +24,7 @@ public class NLMeansDenoisingTest {
 		assertArrayEquals(expectedPixels, nlOfInput);
 	}
 	
+	@Test
 	public void applyNonLocalMeansTest_HigherThanBroadImage() {
 		URL url = this.getClass().getClassLoader().getResource("higher.png");
 		ImagePlus input = new ImagePlus(url.getPath());
@@ -36,13 +37,14 @@ public class NLMeansDenoisingTest {
 		assertArrayEquals(expectedPixels, nlOfInput);
 	}
 	
+	@Test
 	public void applyNonLocalMeansTest_BroaderThanHighImage() {
 		URL url = this.getClass().getClassLoader().getResource("broader.png");
 		ImagePlus input = new ImagePlus(url.getPath());
 		NLMeansDenoising_ nlm = new NLMeansDenoising_();
-		nlm.applyNonLocalMeans(input.getProcessor(), 20);
+		nlm.applyNonLocalMeans(input.getProcessor(), 15);
 		byte[] nlOfInput = (byte[]) input.getProcessor().getPixels();
-		url = this.getClass().getClassLoader().getResource("Result.png");
+		url = this.getClass().getClassLoader().getResource("broaderResult.png");
 		ImagePlus result = new ImagePlus(url.getPath());
 		byte[] expectedPixels = (byte[]) result.getProcessor().getPixels(); 
 		assertArrayEquals(expectedPixels, nlOfInput);
